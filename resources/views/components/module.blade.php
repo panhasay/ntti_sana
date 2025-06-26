271ce27c22275943282db456bfeb12a12304165c
dev-404 <developer251121@gmail.com>
2025-06-26 19:35:50 +0700

    update card student bg

--- resources/views/components/module.blade.php
+++ resources/views/components/module.blade.php
@@ -22,7 +22,6 @@
         left: 0;
         width: 100%;
         height: 100%;
-        background: rgba(0, 0, 0, 0.6);
         color: #fff;
         display: flex;
         align-items: center;
@@ -37,15 +36,79 @@
         opacity: 1;
         color: #fff;
     }
+
+    .ntti-effect-9 {
+        position: relative;
+        margin-bottom: 30px;
+        overflow: hidden;
+        height: 260px;
+        box-shadow: 0 -1px 3px #959595
+    }
+
+    .ntti-effect-9 .effect-img {
+        position: relative;
+        font-size: 0;
+        overflow: hidden
+    }
+
+    .ntti-effect-9 .effect-img img {
+        position: relative;
+        width: 100%;
+        height: 200px;
+        transition: .5s
+    }
+
+    .ntti-effect-9:hover .effect-img img {
+        transform: scale(1.2)
+    }
+
+    .ntti-effect-9 .effect-text {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        padding: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        background: #0000001a;
+        transition: .5s
+    }
+
+    .ntti-effect-9 .effect-text h2 {
+        color: #fff;
+        font-size: 25px;
+        margin-bottom: 15px
+    }
+
+    .ntti-effect-9 .effect-text p {
+        color: #222;
+        font-size: 16px;
+        padding: 0 30px;
+        margin: 0;
+        opacity: 0;
+        transform: scale(0);
+        transition: .5s;
+        transition-delay: .2s
+    }
+
+    .ntti-effect-9:hover .effect-detail {
+        width: 100%;
+        height: calc(100% - 45px)
+    }
+
+    .ntti-effect-9:hover .effect-detail p {
+        opacity: 1;
+        transform: scale(1)
+    }
 </style>
 
 <div {{ $attributes->merge(['class' => 'col-lg-2 col-md-2 col-sm-12']) }}>
-    <div class="effect-9">
+    <div class="ntti-effect-9">
         <div class="effect-img position-relative">
-            <img src="{{ $img }}" alt="{{ $title }}" class="img-fluid rounded">
-            <a href="{{ url($url) }}" class="title-overlay text-center">
-                {{ $title }}
-            </a>
+            <img src="{{ $img }}" alt="{{ $title }}" class="img-fluid">
         </div>
         <div
             style="text-align: center !important;padding: 15px 10px 21px 10px;font-size: 13px;font-family: Khmer OS Battambang !important;font-weight: 900;">
