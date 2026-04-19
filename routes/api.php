405d0530f16ca123cc8d0aba4581ceb1af5411d7
SAY PANHA <121386307+panhasay@users.noreply.github.com>
2026-04-19 18:14:26 +0700

    Add new route for get-student-intti

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('get-student', [ApiController::class, 'APIGetStudent']);
+
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
