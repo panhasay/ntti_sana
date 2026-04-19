57720daaeb6125f432f24d9434f4e82cafaea6db
SAY PANHA <saypanha500@gmail.com>
2026-04-19 18:21:13 +0700

    Route

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('get-student', [ApiController::class, 'APIGetStudent']);
 
 Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
+Route::get('get-student-intti', [ApiController::class, 'APIGetStudent']);
